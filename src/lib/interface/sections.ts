1b859c9889346f85d6b2f40a2a5938bc379d773e
RobsonTrasel <robson.trasel@gmail.com>
2024-01-21 16:55:53 -0300

    chore(sections): add new interfaces using previous types
    Signed-off-by: RobsonTrasel <robson.trasel@gmail.com>

---
+++ src/lib/interface/sections.ts
@@ -0,0 +1,81 @@
+import {
+  FeaturedProjectType
+} from '@/lib/interface/index';
+import {
+  CTAType,
+  ExperienceType,
+  NavLinkType,
+  ProjectType,
+  SocialLinkType,
+  SoftwareSkillType,
+} from '@/lib/types';
+
+export interface Section {
+  title: string
+}
+
+export interface NavbarSectionType {
+  navLinks: NavLinkType[]
+  cta: CTAType
+}
+
+type SkillType = {
+  id: string;
+  title: string;
+  lottie: {
+    light: string;
+    dark: string;
+  };
+  points: string[];
+  softwareSkills: SoftwareSkillType[];
+};
+
+export interface SkillsSectionType extends Section {
+  skills: SkillType[];
+}
+
+export interface ExperienceSectionType extends Section {
+  experiences: ExperienceType[];
+}
+
+export interface ProjectsSectionType extends Section {
+  projects: ProjectType[];
+}
+
+export interface FeaturedProjectsSectionType extends Section {
+  projects: FeaturedProjectType[];
+}
+
+export interface ContactSectionType extends Section {
+  subtitle?: string;
+  paragraphs: string[];
+  link: string;
+}
+
+export interface SocialSectionType {
+  socialLinks: SocialLinkType[];
+}
+
+export interface FooterSectionType extends Section {
+  link: string;
+}
+
+export interface HeroSectionType extends Section {
+  subtitle?: string
+  tagline: string
+  description?: string
+  specialText?: string
+  cta?: CTAType & {
+    hideInDesktop?: boolean
+  }
+}
+
+export interface AboutSectionType extends Section {
+  list: {
+    title: string;
+    items: string[];
+  };
+  img: string;
+}
+
+
