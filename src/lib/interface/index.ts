01a96a4da5519595053f9c54fb61cc074159433d
RobsonTrasel <robson.trasel@gmail.com>
2024-01-21 15:11:00 -0300

    chore(interface): add a new lib for interface

---
+++ src/lib/interface/index.ts
@@ -0,0 +1,7 @@
+import { ProjectType } from "@/lib/types";
+
+export interface FeaturedProjectType extends Omit<ProjectType, 'year' | 'repo'> {
+    description: string
+    repo?: string
+    tasks?: string
+}
